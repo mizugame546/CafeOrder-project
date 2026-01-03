3f1186c88bd562fbe8353104f67b7ebd11ebfe97
owner <owner@DESKTOP-9DRJR0P>
2026-01-04 04:41:30 +0900

    初回コミット: ソースコード、pom.xml、Maven Wrapper、GitHubにあげるためのダミーファイルを追加

---
+++ src/main/java/com/example/cafeorder/CafeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOrderApplication.class, args);
+	}
+
+}
