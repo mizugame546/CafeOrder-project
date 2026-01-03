01ac490f0af713e251103f9aa1978f9d82df565d
owner <owner@DESKTOP-9DRJR0P>
2026-01-04 04:52:09 +0900

    Thymeleafとcontrollerを編集

--- src/main/java/com/example/cafeorder/controller/CafeOrderController.java
+++ src/main/java/com/example/cafeorder/controller/CafeOrderController.java
@@ -1,13 +1,29 @@
 package com.example.cafeorder.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class CafeOrderController {
 
-    @GetMapping("/")
-    public String home() {
-        return "カフェへようこそ！";
+
+    // 注文フォームを表示
+    @GetMapping("/order")
+    public String showOrderForm(Model model) {
+        model.addAttribute("message", "カフェ注文フォームへようこそ！");
+        return "order";
+    }
+
+    // フォームから送信された注文を保存
+    @PostMapping("/order")
+    public String submitOrder(
+        @RequestParam("item") String item,
+    	@RequestParam(value = "quantity", defaultValue = "1") int quantity, Model model) {
+    model.addAttribute("message", "ご注文ありがとうございます！ 選んだ商品: " + item + quantity + "点");
+        return "order";
     }
 }
